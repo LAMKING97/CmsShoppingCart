7b05c593a7b5f5b7edade974056a481c1b2bd0c4
Omar Al-lamki <allamki97@hotmail.com>
2020-07-15 12:35:16 +0400

    db

--- CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
+++ CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CmsShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +9,13 @@ namespace CmsShoppingCart.Infrastructure
 {
     public class CmsShoppingCartContext: DbContext
     {
-
         public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options) : base(options)
         {
 
         }
+
+        public DbSet<Page> Pages { set; get; }
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<Product> Products { set; get; }
     }
 }
